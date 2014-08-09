2e9ae69551b1b9415ae6fae097e9f2a9c749d73d
nikue <nikueh@gmail.com>
2014-08-09 11:17:07 +0300

    Update pdf_renderer.rb

--- lib/payday/pdf_renderer.rb
+++ lib/payday/pdf_renderer.rb
@@ -20,7 +20,7 @@ module Payday
 
         # set up some default styling
         pdf.font_size(8)
-        pdf.font('Times-Roman')
+        pdf.font('/app/lib/fonts/times.ttf')
         
         stamp(invoice, pdf)
         company_banner(invoice, pdf)
