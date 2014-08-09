2573e672a84b22cdf5dfd99afcfd36b551d94777
nikue <nikueh@gmail.com>
2014-08-09 12:21:24 +0300

    Update pdf_renderer.rb

--- lib/payday/pdf_renderer.rb
+++ lib/payday/pdf_renderer.rb
@@ -184,9 +184,13 @@ module Payday
       def self.line_items_table(invoice, pdf)
         table_data = []
         table_data << [bold_cell(pdf, I18n.t('payday.line_item.description', :default => "Description"), :borders => []),
+            bold_cell(pdf, I18n.t('payday.line_item.unit_price', :default => "Unit Price"), :align => :center, :borders => []),
+            bold_cell(pdf, I18n.t('payday.line_item.quantity', :default => "Quantity"), :align => :center, :borders => []),
             bold_cell(pdf, I18n.t('payday.line_item.amount', :default => "Amount"), :align => :center, :borders => [])]
         invoice.line_items.each do |line|
           table_data << [line.description,
+                         (line.display_price || number_to_currency(line.price, invoice)),
+                         (line.display_quantity || BigDecimal.new(line.quantity.to_s).to_s("F")),
                          number_to_currency(line.amount, invoice)]
         end
 
