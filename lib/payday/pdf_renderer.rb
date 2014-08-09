cb74f8f522569b95355088040b7257f7f8b3bb16
nikue <nikueh@gmail.com>
2014-08-09 11:36:07 +0300

    Update pdf_renderer.rb

--- lib/payday/pdf_renderer.rb
+++ lib/payday/pdf_renderer.rb
@@ -21,10 +21,10 @@ module Payday
         # set up some default styling
         pdf.font_size(8)
         pdf.font_families.update("Times" => {
-            :normal => "lib/fonts/times.ttf'",
-            :italic => "lib/fonts/timesi.ttf'",
-            :bold => "lib/fonts/timesb.ttf'",
-            :bold_italic => "lib/fonts/timesbi.ttf'"
+            :normal => "lib/fonts/times.ttf",
+            :italic => "lib/fonts/timesi.ttf",
+            :bold => "lib/fonts/timesb.ttf",
+            :bold_italic => "lib/fonts/timesbi.ttf"
           })
         pdf.font('Times')
         
